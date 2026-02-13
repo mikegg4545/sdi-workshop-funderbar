8efc10dcc9f1a506591328335e1562c706d22f8d
mikegg4545 <42780320+mikegg4545@users.noreply.github.com>
2026-02-13 05:51:14 -0700

    Add for if/else loops

--- spec/part1_variables.js
+++ spec/part1_variables.js
@@ -1,71 +1,63 @@
-(function() {
-  'use strict';
+(function () {
+  "use strict";
 
-  var FILL_ME_IN = 'Fill this value in';
+  var FILL_ME_IN = "Fill this value in";
 
-
-  describe('Part I --- VARIABLES', function() {
-
-    describe('define', function() {
-
-      it('should return whatever value is passed into it', function() {
+  describe("Part I --- VARIABLES", function () {
+    describe("define", function () {
+      it("should return whatever value is passed into it", function () {
         expect(_.define(1)).to.equal(1);
         expect(_.define(false)).to.be.false;
-        expect(_.define('string')).to.equal('string');
+        expect(_.define("string")).to.equal("string");
       });
     });
 
-    describe('dataType', function() {
-
-      it('should return the data type of the value passed into it', function() {
-        expect(_.dataType(1)).to.eql('number');
-        expect(_.dataType(false)).to.eql('boolean');
-        expect(_.dataType('Data Type')).to.equal('string');
-        expect(_.dataType(undefined)).to.equal('undefined');
+    describe("dataType", function () {
+      it("should return the data type of the value passed into it", function () {
+        expect(_.dataType(1)).to.eql("number");
+        expect(_.dataType(false)).to.eql("boolean");
+        expect(_.dataType("Data Type")).to.equal("string");
+        expect(_.dataType(undefined)).to.equal("undefined");
       });
     });
 
-    describe('sum', function() {
-
-      it('should return sum of the two numbers passed into it', function() {
-        expect(_.sum(1,1)).to.eql(2);
+    describe("sum", function () {
+      it("should return sum of the two numbers passed into it", function () {
+        expect(_.sum(1, 1)).to.eql(2);
         expect(_.sum(33, 150)).to.eql(183);
-        expect(_.sum(10,-5)).to.eql(5);
+        expect(_.sum(10, -5)).to.eql(5);
       });
     });
 
-    describe('subtract', function() {
+    describe("subtract", function () {
       //For the tests ensure you are subtracting the second parameter from the first
       //_.subtract(a,b) === a - b
-      it('should return difference of the two numbers passed into it', function() {
-        expect(_.subtract(1,1)).to.eql(0);
+      it("should return difference of the two numbers passed into it", function () {
+        expect(_.subtract(1, 1)).to.eql(0);
         expect(_.subtract(222, 62)).to.eql(160);
-        expect(_.subtract(10,-5)).to.eql(15);
+        expect(_.subtract(10, -5)).to.eql(15);
       });
     });
 
-    describe('multiply', function() {
-
-      it('should return product of the two numbers passed into it', function() {
-        expect(_.multiply(1,1)).to.eql(1);
+    describe("multiply", function () {
+      it("should return product of the two numbers passed into it", function () {
+        expect(_.multiply(1, 1)).to.eql(1);
         expect(_.multiply(11, 5)).to.eql(55);
-        expect(_.multiply(10,-2)).to.eql(-20);
+        expect(_.multiply(10, -2)).to.eql(-20);
       });
     });
 
-    describe('divide', function() {
-
-      it('should return quotient of the two numbers passed into it', function() {
-        expect(_.divide(1,1)).to.eql(1);
+    describe("divide", function () {
+      it("should return quotient of the two numbers passed into it", function () {
+        expect(_.divide(1, 1)).to.eql(1);
         expect(_.divide(144, 12)).to.eql(12);
-        expect(_.divide(65,10)).to.eql(6.5);
+        expect(_.divide(65, 10)).to.eql(6.5);
       });
     });
 
-    describe('remainderOperator', function() {
-
-      it('should return remainderOperator of the two numbers passed into it', function() {
-        expect(_.remainderOperator(1,1)).to.eql(0);
+    describe("remainderOperator", function () {
+      it("should return remainderOperator of the two numbers passed into it", function () {
+        expect(_.remainderOperator(1, 1)).to.eql(0);
         expect(_.remainderOperator(37, 10)).to.eql(7);
         expect(_.remainderOperator(167, 12)).to.eql(11);
         expect(_.remainderOperator(99, 100)).to.eql(99);
@@ -76,45 +68,41 @@
       });
     });
 
-    describe('increment', function() {
-
-      it('should return the number passed in to it incremented by 1', function() {
+    describe("increment", function () {
+      it("should return the number passed in to it incremented by 1", function () {
         expect(_.increment(1)).to.eql(2);
         expect(_.increment(37)).to.eql(74);
         expect(_.increment(167)).to.eql(334);
       });
     });
 
-    describe('decrement', function() {
-
-      it('should return the the number passed in to it decremented by 1', function() {
+    describe("decrement", function () {
+      it("should return the the number passed in to it decremented by 1", function () {
         expect(_.decrement(1)).to.eql(0);
         expect(_.decrement(37)).to.eql(0);
         expect(_.decrement(167)).to.eql(0);
       });
     });
 
-
     //Precedence of Evaluation
-    describe('mathematical order', function() {
+    describe("mathematical order", function () {
       //Based on the test passed into _.mathOrder, determine the value of the output.
       //Feel free to use a calculator if necessary to solve the problem in the correct order.
-      it('determine the outcome based on the equation passed.', function() {
-        var test1 = (a,b,c) => a + b * c;
-        expect(_.mathOrder(test1(1,2,3))).to.eql(7);
+      it("determine the outcome based on the equation passed.", function () {
+        var test1 = (a, b, c) => a + b * c;
+        expect(_.mathOrder(test1(1, 2, 3))).to.eql(7);
 
-        var test2 = (a,b,c) => a * b + c;
-        expect(_.mathOrder(test2(1,2,3))).to.eql(5);
+        var test2 = (a, b, c) => a * b + c;
+        expect(_.mathOrder(test2(1, 2, 3))).to.eql(5);
 
-        var test3 = (a,b,c) => a / c * b + c;
-        expect(_.mathOrder(test3(6,8,2))).to.eql(26);
+        var test3 = (a, b, c) => (a / c) * b + c;
+        expect(_.mathOrder(test3(6, 8, 2))).to.eql(26);
 
-        var test4 = (a,b,c) => a + c + b + b * a / b - c / b;
-        expect(_.mathOrder(test4(10,2,6))).to.eql(25);
+        var test4 = (a, b, c) => a + c + b + (b * a) / b - c / b;
+        expect(_.mathOrder(test4(10, 2, 6))).to.eql(25);
       });
     });
 
     //END OF PART I
   });
-
-}());
+})();
--- src/funderbar.js
+++ src/funderbar.js
@@ -1,239 +1,278 @@
 (function () {
-	'use strict';
+  "use strict";
 
-	window._ = {};
+  window._ = {};
 
-	var FILL_ME_IN = 'Fill this value in';
+  var FILL_ME_IN = "Fill this value in";
 
-	//_.define is wanting you to declare a variable, set your variable to the input from the function
-	// and then return the variable you just created. This function is nearly completed for you,
-	// to help guide you toward success in your work on the following exercises.
-	_.define = function (input) {
-		// Declare a variable below that is assigned the value of input;
-		var result = input;
-		// return the above variable
-		return result;
-	};
+  //_.define is wanting you to declare a variable, set your variable to the input from the function
+  // and then return the variable you just created. This function is nearly completed for you,
+  // to help guide you toward success in your work on the following exercises.
+  _.define = function (input) {
+    // Declare a variable below that is assigned the value of input;
+    var result = input;
+    // return the above variable
+    return result;
+  };
 
-	//_.dataType is wanting you to determine the data type of what is being passed into it.
-	// Return the datatype of input: ie. string or number or boolean etc.
-	_.dataType = function (input) {
-		//Code Goes Below
-		return typeof input;
-	};
+  //_.dataType is wanting you to determine the data type of what is being passed into it.
+  // Return the datatype of input: ie. string or number or boolean etc.
+  _.dataType = function (input) {
+    //Code Goes Below
+    return typeof input;
+  };
 
-	//_.sum takes two numbers as parameters and returns their sum
-	_.sum = function (a, b) {
-		//Code Goes Below
-		return a + b;
-	};
+  //_.sum takes two numbers as parameters and returns their sum
+  _.sum = function (a, b) {
+    //Code Goes Below
+    return a + b;
+  };
 
-	//_.subtract takes two numbers as parameters and returns their difference
-	_.subtract = function (a, b) {
-		//Code Goes Below
-		//For your code remember to subtract b from a
-		//a - b
-		return a - b;
-	};
+  //_.subtract takes two numbers as parameters and returns their difference
+  _.subtract = function (a, b) {
+    //Code Goes Below
+    //For your code remember to subtract b from a
+    //a - b
+    return a - b;
+  };
 
-	//_.multiply takes two numbers as parameters and returns their product
-	_.multiply = function (a, b) {
-		//Code Goes Below
-		return a * b;
-	};
+  //_.multiply takes two numbers as parameters and returns their product
+  _.multiply = function (a, b) {
+    //Code Goes Below
+    return a * b;
+  };
 
-	//_.divide takes two numbers as parameters and returns their quotient
-	_.divide = function (a, b) {
-		//Code Goes Below
-		//a divided by b
-		return a / b;
-	};
+  //_.divide takes two numbers as parameters and returns their quotient
+  _.divide = function (a, b) {
+    //Code Goes Below
+    //a divided by b
+    return a / b;
+  };
 
-	//_.remainderOperator takes two numbers as parameters and returns their remainder of a divided by b
-	_.remainderOperator = function (a, b) {
-		//Code Goes Below
-		//return the remainder of a divided by b
-		return a % b;
-	};
+  //_.remainderOperator takes two numbers as parameters and returns their remainder of a divided by b
+  _.remainderOperator = function (a, b) {
+    //Code Goes Below
+    //return the remainder of a divided by b
+    return a % b;
+  };
 
-	//_.increment takes a number as a parameter and increces it by 1
-	// Ensure you use the proper increment operator(not a+1)
-	_.increment = function (a) {
-		//Code Goes Below
-		return (a += a);
-	};
+  //_.increment takes a number as a parameter and increces it by 1
+  // Ensure you use the proper increment operator(not a+1)
+  _.increment = function (a) {
+    //Code Goes Below
+    return (a += a);
+  };
 
-	//_.decrement takes a number as a parameter and reduces it by 1
-	// Ensure you use the proper decrement operator (not a-1)
-	_.decrement = function (a) {
-		//Code Goes Below
-		return (a -= a);
-	};
+  //_.decrement takes a number as a parameter and reduces it by 1
+  // Ensure you use the proper decrement operator (not a-1)
+  _.decrement = function (a) {
+    //Code Goes Below
+    return (a -= a);
+  };
 
-	//This function is complete and working. All you will need to do is
-	//replace FILL_ME_IN in the variables.js test file.
+  //This function is complete and working. All you will need to do is
+  //replace FILL_ME_IN in the variables.js test file.
 
-	_.mathOrder = function (test) {
-		return test;
-	};
+  _.mathOrder = function (test) {
+    return test;
+  };
 
-	//PART II
+  //PART II
 
-	//-------------------------------------------------------------------------
-	/*
+  //-------------------------------------------------------------------------
+  /*
   Now you will start using logic to pass your tests. You will be using comparators to pass the tests.
 */
-	//-------------------------------------------------------------------------
+  //-------------------------------------------------------------------------
 
-	//_.same takes two parameters and returns whether they are the equal or not
-	_.same = function (a, b) {
-		//Code Goes Below
-		return a === b;
-	};
+  //_.same takes two parameters and returns whether they are the equal or not
+  _.same = function (a, b) {
+    //Code Goes Below
+    return a === b;
+  };
 
-	//_.greater takes two numbers as parameters and checks if a is greater than b
-	_.greater = function (a, b) {
-		//Code Goes Below
-		return a > b;
-	};
+  //_.greater takes two numbers as parameters and checks if a is greater than b
+  _.greater = function (a, b) {
+    //Code Goes Below
+    return a > b;
+  };
 
-	//_.lessThan takes two numbers as parameters and checks if a is less than b
-	_.lessThan = function (a, b) {
-		//Code Goes Below
-		return a < b;
-	};
+  //_.lessThan takes two numbers as parameters and checks if a is less than b
+  _.lessThan = function (a, b) {
+    //Code Goes Below
+    return a < b;
+  };
 
-	//_.falsyOrTruthy takes a parameter and determines whether it is TRUTHY or FALSY
-	_.falsyOrTruthy = function (a) {
-		//Code Goes Below
-		return a === true;
-	};
+  //_.falsyOrTruthy takes a parameter and determines whether it is TRUTHY or FALSY
+  _.falsyOrTruthy = function (a) {
+    //Code Goes Below
+    return a === true;
+  };
 
-	//In the next tests you will be given four parameters to compare, sort of.
-	//You will be given a, b, c, and d. Within your function you will compare
-	//a to b as well as comparing c to d. You will then compare their outcomes with
-	//each other using the && or || logic to return true or false.
-	//EXAMPLE 3 > 2 && 5 === 6
-	//The above example will evaluate to False because 3 > 2 === True and 5 === 6 False,
-	// as you learned in the Learn content True && False evaluates to False.
-	//For the _.andAnd function as well as the _.oror function you will just be using the === comparator
+  //In the next tests you will be given four parameters to compare, sort of.
+  //You will be given a, b, c, and d. Within your function you will compare
+  //a to b as well as comparing c to d. You will then compare their outcomes with
+  //each other using the && or || logic to return true or false.
+  //EXAMPLE 3 > 2 && 5 === 6
+  //The above example will evaluate to False because 3 > 2 === True and 5 === 6 False,
+  // as you learned in the Learn content True && False evaluates to False.
+  //For the _.andAnd function as well as the _.oror function you will just be using the === comparator
 
-	//_.andAnd will be using the && comparator
-	_.andAnd = function (a, b, c, d) {
-		//Code Goes Below
-		return a === b && c === d;
-	};
+  //_.andAnd will be using the && comparator
+  _.andAnd = function (a, b, c, d) {
+    //Code Goes Below
+    return a === b && c === d;
+  };
 
-	//_.orOr will be using the || comparator
-	_.orOr = function (a, b, c, d) {
-		//Code Goes Below
-		return a === b || c === d;
-	};
+  //_.orOr will be using the || comparator
+  _.orOr = function (a, b, c, d) {
+    //Code Goes Below
+    return a === b || c === d;
+  };
 
-	//Control Flow
-	//Create a basic if statement to pass the tests.
-	_.ifLogic = function (a, b) {
-		//Code Goes Below
-		if (a === b) {
-			return 'Great';
-		}
-	};
+  //Control Flow
+  //Create a basic if statement to pass the tests.
+  _.ifLogic = function (a, b) {
+    //Code Goes Below
+    if (a === b) {
+      return "Great";
+    }
+  };
 
-	//Create a basic if else statement to pass the tests.
-	_.ifElseLogic = function (a, b) {
-		//Code Goes Below
-		if (a === b) {
-			return 'Great';
-		} else {
-			return 'Good';
-		}
-	};
+  //Create a basic if else statement to pass the tests.
+  _.ifElseLogic = function (a, b) {
+    //Code Goes Below
+    if (a === b) {
+      return "Great";
+    } else {
+      return "Good";
+    }
+  };
 
-	//Using a for loop, iterate through the array passed.
-	//Return the array as a single string.
-	_.forLoop = function (a) {
-		//Code Goes Below
-		let it = '';
-		for (let i = 0; i < a.length; i++) {
-			it += a[i];
-		}
-		return it;
-	};
+  //Using a for loop, iterate through the array passed.
+  //Return the array as a single string.
+  _.forLoop = function (a) {
+    //Code Goes Below
+    let it = "";
+    for (let i = 0; i < a.length; i++) {
+      it += a[i];
+    }
+    return it;
+  };
 
-	//Using NESTED for loops, iterate through an array or arrays
-	_.nestedForLoop = function (a) {
-		//Code Goes Below
-		let total = 0;
-		for (let i = 0; i < a.length; i++) {
-			for (let j = 0; j < a[i].length; j++) {
-				total += a[i][j];
-			}
-		}
-		return total;
-	};
+  //Using NESTED for loops, iterate through an array or arrays
+  _.nestedForLoop = function (a) {
+    //Code Goes Below
+    let total = 0;
+    for (let i = 0; i < a.length; i++) {
+      for (let j = 0; j < a[i].length; j++) {
+        total += a[i][j];
+      }
+    }
+    return total;
+  };
 
-	//While looping through the array passed determine the data type of the element. If the element is a number add it to a variable. At the end return that variable.
-	//If an index in the array is not a number add 1 to the above mentioned variable
-	_.forIfElseNum = function (a) {
-		//Code Goes Below
-	};
+  //While looping through the array passed determine the data type of the element. If the element is a number add it to a variable. At the end return that variable.
+  //If an index in the array is not a number add 1 to the above mentioned variable
+  _.forIfElseNum = function (a) {
+    //Code Goes Below
+    let total = 0;
 
-	//This function is similar to the above function. Only this time you will be building a sentence rather than a total number. Loop through the array and concatenate the strings in it to make a sentence.
-	//If an index in the array is not a string add and empty string ("") to the local variable
-	_.forIfElseStr = function (a) {
-		//Code Goes Below
-	};
+    // for (let i = 0; i < a.length; i++) {
+    //   if (typeof a[i] === "number") {
+    //     total += a[i];
+    //   } else {
+    //     total += 1;
+    //   }
+    // }
 
-	//In this function you will loop though the passed in array. Prior to that you need to check the second parameter to determine what you will be returning.
-	//If the second parameter is 'string' return a sentence created using the strings in the array. If the second parameter is 'number' you will return the total of the number in the array.
-	//You may notice you did this work already in previous functions but need to combine them in some way. Ideally, you should call the previous two functions in your implementation instead of copy/pasting the 'guts'.
-	_.ifElseFor = function (a, b) {
-		//Code Goes Below
-	};
+    for (let value of a) {
+      if (typeof value === "number") {
+        total += value;
+      } else {
+        total += 1;
+      }
+    }
 
-	//PART III
-	//-------------------------------------------------------------------------
-	/*
+    return total;
+  };
+
+  //This function is similar to the above function. Only this time you will be building a sentence rather than a total number. Loop through the array and concatenate the strings in it to make a sentence.
+  //If an index in the array is not a string add and empty string ("") to the local variable
+  _.forIfElseStr = function (a) {
+    //Code Goes Below
+    let sentence = "";
+
+    for (let i = 0; i < a.length; i++) {
+      if (typeof a[i] === "string") {
+        sentence += a[i];
+      } else {
+        sentence += "";
+      }
+    }
+
+    return sentence;
+  };
+
+  //In this function you will loop though the passed in array. Prior to that you need to check the second parameter to determine what you will be returning.
+  //If the second parameter is 'string' return a sentence created using the strings in the array. If the second parameter is 'number' you will return the total of the number in the array.
+  //You may notice you did this work already in previous functions but need to combine them in some way. Ideally, you should call the previous two functions in your implementation instead of copy/pasting the 'guts'.
+  _.ifElseFor = function (a, b) {
+    //Code Goes Below
+    // if (b === "string") {
+    //   return _.forIfElseStr(a);
+    // }
+    // if (b === "number") {
+    //   return _.forIfElseNum(a);
+    // }
+
+    //If b equals "string", return the string version. Otherwise, return the number version.
+    return b === "string" ? _.forIfElseStr(a) : _.forIfElseNum(a);
+  };
+
+  //PART III
+  //-------------------------------------------------------------------------
+  /*
   Part III will be using arrow functions rather than the traditional function implementation. Note the similarities and differences.
 */
-	//-------------------------------------------------------------------------
+  //-------------------------------------------------------------------------
 
-	//Below you will be building a function that creates an object using bracket notation ([]). Within the function create and return an object.
+  //Below you will be building a function that creates an object using bracket notation ([]). Within the function create and return an object.
 
-	_.objectCreationBracket = (a, b, c) => {
-		//Code Goes Below
-	};
+  _.objectCreationBracket = (a, b, c) => {
+    //Code Goes Below
+  };
 
-	//Below you will be building a function that creates an object using dot notation (.). Within the function create and return an object.
+  //Below you will be building a function that creates an object using dot notation (.). Within the function create and return an object.
 
-	_.objectCreationDot = (a, b, c) => {
-		//Code Goes Below
-	};
+  _.objectCreationDot = (a, b, c) => {
+    //Code Goes Below
+  };
 
-	_.objectValue = (a, b) => {
-		//Code Goes Below
-	};
+  _.objectValue = (a, b) => {
+    //Code Goes Below
+  };
 
-	//Below you are going to loop through an array, only this time you need to use a for...of loop rather than an traditional for loop.
-	//The tests will be similar to the last time you looped through an array.
-	//What you will be doing is called refactoring. You take working code and restructure it, either for readability or efficiency, or any other countless number of reasons.
-	//NOTE: You can also use the same logic in a for...of loop as you would use in a traditional for loop.
+  //Below you are going to loop through an array, only this time you need to use a for...of loop rather than an traditional for loop.
+  //The tests will be similar to the last time you looped through an array.
+  //What you will be doing is called refactoring. You take working code and restructure it, either for readability or efficiency, or any other countless number of reasons.
+  //NOTE: You can also use the same logic in a for...of loop as you would use in a traditional for loop.
 
-	//Loop through the given array and return the total sum of all the numbers in the array.
-	_.forOf = (a) => {
-		//Code Goes Below
-	};
+  //Loop through the given array and return the total sum of all the numbers in the array.
+  _.forOf = (a) => {
+    //Code Goes Below
+  };
 
-	//Loop through the given object and return an array of keys from the object.
-	//Note: to add values to an array use .push();
-	//example array.push(value)
-	_.forInKeys = (a) => {
-		//Code Goes Below
-	};
+  //Loop through the given object and return an array of keys from the object.
+  //Note: to add values to an array use .push();
+  //example array.push(value)
+  _.forInKeys = (a) => {
+    //Code Goes Below
+  };
 
-	//Loop through the given object and return an array of values from the object.
-	//This is similar to the above function but needs a little digging to get the value.
-	_.forInValues = (a) => {
-		//Code Goes Below
-	};
+  //Loop through the given object and return an array of values from the object.
+  //This is similar to the above function but needs a little digging to get the value.
+  _.forInValues = (a) => {
+    //Code Goes Below
+  };
 })();
